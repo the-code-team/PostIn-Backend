6bba5f942c1575b2b1c1c84b380c7f9393aab5d4
Sergio Martinez <7413024+Neirth@users.noreply.github.com>
2024-05-23 11:51:58 +0200

    fix: Accepted suggestions by @coderabbitai

--- src/models/Profile.go
+++ src/models/Profile.go
@@ -1,11 +1,11 @@
 package models
 
 type Profile struct {
-	UserId         string `gorm:"primaryKey",json:"user_id","omitempty"`
-	Email          string `json:"email","omitempty"`
-	FirstName      string `json:"first_name","omitempty"`
-	LastName       string `json:"last_name","omitempty"`
-	ProfilePicture string `json:"profile_picture","omitempty"`
+	UserId         string `gorm:"primaryKey" json:"user_id,omitempty"`
+	Email          string `json:"email,omitempty"`
+	FirstName      string `json:"first_name,omitempty"`
+	LastName       string `json:"last_name,omitempty"`
+	ProfilePicture string `json:"profile_picture,omitempty"`
 
-	Tag []Tag `json:"tag","omitempty"`
+	Tag []Tag `json:"tag,omitempty"`
 }
--- src/models/Propose.go
+++ src/models/Propose.go
@@ -13,10 +13,10 @@ const (
 )
 
 type Propose struct {
-	UserId  string        	`gorm:"primaryKey",json:"user_id","omitempty"`
-	EventId uuid.UUID     	`gorm:"primaryKey",json:"event_id","omitempty"`
-	Status  ProposeStatus 	`gorm:"default:pending",json:"status","omitempty"`
+	UserId  string        `gorm:"primaryKey" json:"user_id,omitempty"`
+	EventId uuid.UUID     `gorm:"primaryKey" json:"event_id,omitempty"`
+	Status  ProposeStatus `gorm:"default:pending" json:"status,omitempty"`
 
-	Profile Profile 		`gorm:"foreignKey:UserId"`
-	Event   Event   		`gorm:"foreignKey:EventId"`
+	Profile Profile `gorm:"foreignKey:UserId"`
+	Event   Event   `gorm:"foreignKey:EventId"`
 }
